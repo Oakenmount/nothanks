cd888a628063869fc1449770938bd286e697e535
Jacob <jacob.egebjerg@gmail.com>
2024-01-22 18:40:38 +0100

    Created basic command-line game

--- gamestate.py
+++ gamestate.py
@@ -1,31 +1,6 @@
 import numpy as np
 
 
-class GameState:
-    def __init__(self, num_players: int = 4):
-        # Generate a deck of cards with numbers 3-35, shuffle and remove 9 random cards
-        self.deck = np.random.shuffle(np.arange(3, 35 + 1))[:-9]
-        self.players = [Player(i) for i in range(num_players)]
-        self.current_turn = 0
-        self.current_card = 0
-        self.chips = 0
-
-    def end_turn(self):
-        self.current_turn = (self.current_turn + 1) % len(self.players)
-
-    def take_card(self):
-        card = self.deck[self.current_card]
-        player = self.players[self.current_turn]
-        player.take(card, self.chips)
-        self.current_card += 1
-        self.end_turn()
-
-    def skip_card(self):
-        player = self.players[self.current_turn]
-        player.skip()
-        self.end_turn()
-
-
 class Player:
     def __init__(self, id: int):
         self.id = id
@@ -44,3 +19,46 @@ class Player:
 
     def score(self):
         return sum(self.cards) - self.chips
+
+
+class GameState:
+    def __init__(self, num_players: int = 4):
+        # Generate a deck of cards with numbers 3-35, shuffle and remove 9 random cards
+        self.deck = np.arange(3, 35 + 1, dtype=np.uint8)
+        np.random.shuffle(self.deck)
+        self.deck = self.deck[:-9]
+        self.players = [Player(i) for i in range(num_players)]
+        self.current_turn = 0
+        self.current_card = 0
+        self.chips = 0
+
+    def end_turn(self):
+        self.current_turn = (self.current_turn + 1) % len(self.players)
+
+    def take_card(self):
+        card = self.deck[self.current_card]
+        player = self.players[self.current_turn]
+        # noinspection PyTypeChecker
+        player.take(card, self.chips)
+        self.chips = 0
+        self.current_card += 1
+        self.end_turn()
+
+    def skip_card(self):
+        player = self.players[self.current_turn]
+        player.skip()
+        self.chips += 1
+        self.end_turn()
+
+    def is_over(self) -> bool:
+        return self.current_card >= len(self.deck)
+
+    def get_player(self) -> Player:
+        return self.players[self.current_turn]
+
+    def get_card(self) -> int:
+        # noinspection PyTypeChecker
+        return self.deck[self.current_card]
+
+    def get_chips(self) -> int:
+        return self.chips
--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-# Hello
+from gamestate import GameState
+
+
+def main():
+    game = GameState(num_players=3)
+
+    while not game.is_over():
+        player = game.get_player()
+        print(f"Current turn: {player.id},  chips: {player.chips}")
+        print(f"Card: {game.get_card()}, chips: {game.get_chips()}")
+        action = input("Select action <Take/Skip> or <Hand> to see cards.\n")
+        if action.lower() == "take":
+            game.take_card()
+        elif action.lower() == "skip":
+            if player.can_skip():
+                game.skip_card()
+            else:
+                print("Player has no chips!")
+        elif action.lower() == "hand":
+            print(player.cards)
+        else:
+            print("Invalid option")
+
+    print("Game over")
+    for player in game.players:
+        print(f"Player {player.id} has {player.score()} points")
+
+
+if __name__ == "__main__":
+    main()
